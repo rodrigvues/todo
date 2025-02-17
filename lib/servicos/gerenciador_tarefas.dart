e1d723d8cb0b798bbf679a00cc9e8b57a6d26355
Vitor Rodrigues <vitor.rodriguess3112@gmail.com>
2025-02-17 12:11:19 -0300

    arrumando /$ e texto

--- lib/servicos/gerenciador_tarefas.dart
+++ lib/servicos/gerenciador_tarefas.dart
@@ -5,7 +5,7 @@ class GerenciadorTarefas {
 
   void adicionarTarefa(String titulo) {
     tarefas.add(Tarefa(titulo));
-    print("Tarefa adicionada: \$titulo");
+    print("Tarefa adicionada: $titulo");
   }
 
   void listarTarefas() {
@@ -16,14 +16,14 @@ class GerenciadorTarefas {
     print("\nLista de Tarefas:");
     for (int i = 0; i < tarefas.length; i++) {
       String status = tarefas[i].concluida ? "Concluída" : "Pendente";
-      print("\${i + 1} - \$status \${tarefas[i].titulo}");
+      print("${i + 1} - ${tarefas[i].titulo} : $status");
     }
   }
 
   void concluirTarefa(int index) {
     if (index >= 0 && index < tarefas.length) {
       tarefas[index].marcarComoConcluida();
-      print("Tarefa concluída: \${tarefas[index].titulo}");
+      print("Tarefa concluída: ${tarefas[index].titulo}");
     } else {
       print("Índice inválido.");
     }
@@ -31,7 +31,7 @@ class GerenciadorTarefas {
 
   void removerTarefa(int index) {
     if (index >= 0 && index < tarefas.length) {
-      print("Tarefa removida: \${tarefas[index].titulo}");
+      print("Tarefa removida: ${tarefas[index].titulo}");
       tarefas.removeAt(index);
     } else {
       print("Índice inválido.");
