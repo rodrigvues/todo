1d4bb07db29184d0290bf2ad289db0f588ebb2d7
Vitor Rodrigues <129552443+rodrigvues@users.noreply.github.com>
2025-02-17 15:08:53 -0300

    documentando

--- lib/principal.dart
+++ lib/principal.dart
@@ -2,8 +2,8 @@ import 'package:todo_list_dart/interface/menu.dart';
 import 'package:todo_list_dart/servicos/gerenciador_tarefas.dart';
 
 void main() {
-  GerenciadorTarefas gerenciador = GerenciadorTarefas();
-  Menu menu = Menu(gerenciador);
-  menu.exibirMenu();
+  GerenciadorTarefas gerenciador = GerenciadorTarefas(); // chamando o gerenciador de tarefa
+  Menu menu = Menu(gerenciador); // chamando o menu
+  menu.exibirMenu(); //inicializando menu
 }
 
